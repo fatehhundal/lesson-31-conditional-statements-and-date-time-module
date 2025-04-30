1c51373d533f7167d841a8dd344f6d1eb6a336d9
fatehhundal <fshundal@hotmail.com>
2025-04-30 18:36:09 +0800

    date_time

---
+++ date_time.py
@@ -0,0 +1,12 @@
+import datetime
+
+# Using now() to get the current time
+current_time = datetime.datetime.now()
+
+# Printing the value of now
+print("The time now at Greenwich Meridan is: ", end = "")
+print(current_time)
+
+# Printing the calendar of 2025
+import calendar
+print("\n", calendar.calendar(2025))
